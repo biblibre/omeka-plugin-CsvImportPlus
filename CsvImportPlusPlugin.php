15304cc77164a452cef599d6012377a80d738304
Daniel Berthereau <Daniel.github@Berthereau.net>
2016-08-22 00:00:00 +0200

    Removed old upgrades.

--- CsvImportPlusPlugin.php
+++ CsvImportPlusPlugin.php
@@ -88,7 +88,7 @@ class CsvImportPlusPlugin extends Omeka_Plugin_AbstractPlugin
         // Create csv imports plus table.
         // Note: CsvImportPlus_Import and CsvImportPlus_ImportedRecord are standard Zend
         // records, but not Omeka ones fully.
-        $db->query("CREATE TABLE IF NOT EXISTS `{$db->prefix}csv_import_plus_imports` (
+        $db->query("CREATE TABLE IF NOT EXISTS `{$db->CsvImportPlus_Import}` (
             `id` int(10) unsigned NOT NULL auto_increment,
             `format` varchar(255) collate utf8_unicode_ci NOT NULL,
             `delimiter` varchar(1) collate utf8_unicode_ci NOT NULL,
@@ -109,7 +109,7 @@ class CsvImportPlusPlugin extends Omeka_Plugin_AbstractPlugin
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;");
 
         // Create csv imported records table.
-        $db->query("CREATE TABLE IF NOT EXISTS `{$db->prefix}csv_import_plus_imported_records` (
+        $db->query("CREATE TABLE IF NOT EXISTS `{$db->CsvImportPlus_ImportedRecord}` (
             `id` int(10) unsigned NOT NULL auto_increment,
             `import_id` int(10) unsigned NOT NULL,
             `record_type` varchar(50) collate utf8_unicode_ci NOT NULL,
@@ -146,160 +146,9 @@ class CsvImportPlusPlugin extends Omeka_Plugin_AbstractPlugin
         $newVersion = $args['new_version'];
         $db = $this->_db;
 
-        // Do this first because MySQL will complain about any ALTERs to a table with an
-        // invalid default if we don't fix it first
-        if (version_compare($oldVersion, '2.2-full', '<=')) {
-            $sql = "ALTER TABLE `{$db->prefix}csv_import_imports` MODIFY `added` timestamp NOT NULL default '2000-01-01 00:00:00'";
-            $db->query($sql);
-        }
-
-        if (version_compare($oldVersion, '2.0-dev', '<=')) {
-            $sql = "UPDATE `{$db->prefix}csv_import_imports` SET `status` = ? WHERE `status` = ?";
-            $db->query($sql, array('other_error', 'error'));
-        }
-
-        if (version_compare($oldVersion, '2.0', '<=')) {
-            set_option(CsvImportPlus_RowIterator::COLUMN_DELIMITER_OPTION_NAME, CsvImportPlus_RowIterator::DEFAULT_COLUMN_DELIMITER);
-            set_option(CsvImportPlus_ColumnMap_Element::ELEMENT_DELIMITER_OPTION_NAME, CsvImportPlus_ColumnMap_Element::DEFAULT_ELEMENT_DELIMITER);
-            set_option(CsvImportPlus_ColumnMap_Tag::TAG_DELIMITER_OPTION_NAME, CsvImportPlus_ColumnMap_Tag::DEFAULT_TAG_DELIMITER);
-            set_option(CsvImportPlus_ColumnMap_File::FILE_DELIMITER_OPTION_NAME, CsvImportPlus_ColumnMap_File::DEFAULT_FILE_DELIMITER);
-            set_option('csv_import_html_elements', $this->_options['csv_import_plus_html_elements']);
-            set_option('csv_import_automap_columns', $this->_options['csv_import_plus_automap_columns']);
-        }
-
-        if (version_compare($oldVersion, '2.0.1', '<=')) {
-            $sql = "
-                ALTER TABLE `{$db->prefix}csv_import_imports`
-                CHANGE `item_type_id` `item_type_id` INT( 10 ) UNSIGNED NULL,
-                CHANGE `collection_id` `collection_id` INT( 10 ) UNSIGNED NULL
-            ";
-            $db->query($sql);
-        }
-
-        if (version_compare($oldVersion, '2.0.3', '<=')) {
-            set_option('csv_import_enclosure', CsvImportPlus_RowIterator::DEFAULT_ENCLOSURE);
-            $sql = "
-                ALTER TABLE `{$db->prefix}csv_import_imports`
-                ADD `format` varchar(255) collate utf8_unicode_ci NOT NULL AFTER `collection_id`,
-                ADD `enclosure` varchar(1) collate utf8_unicode_ci NOT NULL AFTER `delimiter`,
-                ADD `row_count` int(10) unsigned NOT NULL AFTER `status`
-            ";
-            $db->query($sql);
-
-            // Update index. Item id is no more unique, because CsvImport can
-            // import files separately, so an item can be updated. Furthermore,
-            // now, any metadata can be updated individualy too.
-            $sql = "
-                ALTER TABLE `{$db->prefix}csv_import_imported_items`
-                ADD `source_item_id` varchar(255) collate utf8_unicode_ci NOT NULL AFTER `item_id`,
-                DROP INDEX `item_id`,
-                ADD INDEX `source_item_id_import_id` (`source_item_id`, `import_id`)
-            ";
-            $db->query($sql);
-        }
-
-        if (version_compare($oldVersion, '2.1.1-full', '<')) {
-            // Move all default values into a specific field.
-            $sql = "
-                ALTER TABLE `{$db->prefix}csv_import_imports`
-                ADD `serialized_default_values` text collate utf8_unicode_ci NOT NULL AFTER `file_path`,
-                ADD `updated_record_count` int(10) unsigned NOT NULL AFTER `skipped_item_count`
-            ";
-            $db->query($sql);
-
-            // Keep previous default values.
-            $table = $db->getTable('CsvImport_Import');
-            $alias = $table->getTableAlias();
-            $select = $table->getSelect();
-            $select->reset(Zend_Db_Select::COLUMNS);
-            $select->from(array(), array(
-                $alias . '.id',
-                $alias . '.item_type_id',
-                $alias . '.collection_id',
-                $alias . '.is_public',
-                $alias . '.is_featured',
-            ));
-            $result = $table->fetchAll($select);
-            $sql = "
-                UPDATE `{$db->prefix}csv_import_imports`
-                SET `serialized_default_values` = ?
-                WHERE `id` = ?
-            ";
-            foreach ($result as $values) {
-                $bind = $values;
-                unset($bind['id']);
-                $db->query($sql, array(serialize($bind), $values['id']));
-            }
-
-            // Reorder columns and change name of "skipped_item_count" column.
-            $sql = "
-                ALTER TABLE `{$db->prefix}csv_import_imports`
-                DROP `item_type_id`,
-                DROP `collection_id`,
-                DROP `is_public`,
-                DROP `is_featured`,
-                CHANGE `format` `format` varchar(255) collate utf8_unicode_ci NOT NULL AFTER `id`,
-                CHANGE `delimiter` `delimiter` varchar(1) collate utf8_unicode_ci NOT NULL AFTER `format`,
-                CHANGE `enclosure` `enclosure` varchar(1) collate utf8_unicode_ci NOT NULL AFTER `delimiter`,
-                CHANGE `status` `status` varchar(255) collate utf8_unicode_ci AFTER `enclosure`,
-                CHANGE `row_count` `row_count` int(10) unsigned NOT NULL AFTER `status`,
-                CHANGE `skipped_row_count` `skipped_row_count` int(10) unsigned NOT NULL AFTER `row_count`,
-                CHANGE `skipped_item_count` `skipped_record_count` int(10) unsigned NOT NULL AFTER `skipped_row_count`,
-                CHANGE `updated_record_count` `updated_record_count` int(10) unsigned NOT NULL AFTER `skipped_record_count`,
-                CHANGE `file_position` `file_position` bigint unsigned NOT NULL AFTER `updated_record_count`,
-                CHANGE `original_filename` `original_filename` text collate utf8_unicode_ci NOT NULL AFTER `file_position`,
-                CHANGE `file_path` `file_path` text collate utf8_unicode_ci NOT NULL AFTER `original_filename`,
-                CHANGE `serialized_default_values` `serialized_default_values` text collate utf8_unicode_ci NOT NULL AFTER `file_path`,
-                CHANGE `serialized_column_maps` `serialized_column_maps` text collate utf8_unicode_ci NOT NULL AFTER `serialized_default_values`,
-                CHANGE `owner_id` `owner_id` int unsigned NOT NULL AFTER `serialized_column_maps`,
-                CHANGE `added` `added` timestamp NOT NULL default '2000-01-01 00:00:00' AFTER `owner_id`
-            ";
-            $db->query($sql);
-
-            $sql = "
-                ALTER TABLE `{$db->prefix}csv_import_imported_items`
-                ADD `record_type` varchar(50) collate utf8_unicode_ci NOT NULL DEFAULT ''  AFTER `id`,
-                CHANGE `import_id` `import_id` int(10) unsigned NOT NULL AFTER `id`,
-                CHANGE `item_id` `record_id` int(10) unsigned NOT NULL AFTER `record_type`,
-                CHANGE `source_item_id` `identifier` varchar(255) COLLATE 'utf8_unicode_ci' NOT NULL AFTER `record_id`,
-                DROP INDEX `source_item_id_import_id`,
-                ADD INDEX  `record_type_record_id` (`record_type`, `record_id`),
-                ADD INDEX  `identifier` (`identifier`),
-                RENAME TO `{$db->prefix}csv_import_imported_records`
-            ";
-            $db->query($sql);
-
-            // Fill all record identifiers as Item.
-            $sql = "UPDATE `{$db->prefix}csv_import_imported_records` SET `record_type` = 'Item'";
-            $db->query($sql);
-        }
-
-        if (version_compare($oldVersion, '2.1.3-full', '<')) {
-            $sql = "
-                CREATE TABLE IF NOT EXISTS `{$db->prefix}csv_import_logs` (
-                    `id` int(10) unsigned NOT NULL auto_increment,
-                    `import_id` int(10) unsigned NOT NULL,
-                    `priority` tinyint unsigned NOT NULL,
-                    `created` timestamp DEFAULT CURRENT_TIMESTAMP,
-                    `message` text NOT NULL,
-                    PRIMARY KEY (`id`),
-                    KEY (`import_id`)
-                ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-            ";
-            $db->query($sql);
-        }
-
-        if (version_compare($oldVersion, '2.1.4-full', '<')) {
-            $sql = "
-                ALTER TABLE `{$db->prefix}csv_import_logs`
-                ADD COLUMN `params` text DEFAULT NULL
-            ";
-            $db->query($sql);
-        }
-
-        if (version_compare($oldVersion, '2.2-full', '<')) {
-            delete_option('csv_import_automap_columns');
-            delete_option('csv_import_create_collections');
+        if (version_compare($oldVersion, '2.3', '<')) {
+            $message = __('There is no upgrade path from the old fork of CSV Import to CSV Import+, because they can be installed simultaneously.');
+            throw new Omeka_Plugin_Exception($message);
         }
     }
 
